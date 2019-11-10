2da427e33c2563db9d691f245948ee21437869fe
swang1997 <wangzicong1997@gmail.com>
2019-11-10 16:36:27 +0800

    Modified input for Export CSV selection

--- src/main/java/duke/logic/commands/ExportLockerSelectCommand.java
+++ src/main/java/duke/logic/commands/ExportLockerSelectCommand.java
@@ -11,8 +11,14 @@ public class ExportLockerSelectCommand extends Command {
     private final String argument;
     public static final String INVALID_FORMAT = " Invalid command format for exporting selection of csv file. "
             + "You must key in 'exports' with tags such as 'Locker,Name,Zone' etc.";
+
     public static final String COMMAND_WORD = "exports";
-    public static final String MISSING_FORMAT = "Serial Number is Mandatory, please input 'Locker'!";
+
+    public static final String MISSINGLOCKER_FORMAT = "Unable to export CSV file without 'Locker' input. "
+            + "You must key in tags with spaces in between. "
+            + "Example: 'exports locker zone status name'";
+
+    public static final String MISSINGSTATUS_FORMAT = "Unable to pull student info without 'Status' input";
 
 
     public ExportLockerSelectCommand(String arg) {
--- src/main/java/duke/parser/ExportLockerSelectCommandParser.java
+++ src/main/java/duke/parser/ExportLockerSelectCommandParser.java
@@ -11,16 +11,60 @@ public class ExportLockerSelectCommandParser {
     /**
      * This function is used to parse the user input for exporting the details as a CSV file.
      * @param args this is the user input string for the tags that the user wants
+     * @return reference to the class ExportLockerSelectCommand
+     * @throws DukeException when the user input is invalid
      */
 
     public ExportLockerSelectCommand parse(String args) throws DukeException {
+
         requireNonNull(args);
+        args = args.toLowerCase();
+
+        int checkLocker = 0;
+        int checkStatus = 0;
+
+        String[] newArgs = args.split(" ");
+
         if (args.trim().length() == 0) {
             throw new DukeException(ExportLockerSelectCommand.INVALID_FORMAT);
+        } else {
+            for (String str : newArgs) {
+                if (str.equals("locker")) {
+                    checkLocker = 1;
+                }
+            }
+            if (checkLocker == 0) {
+                throw new DukeException(ExportLockerSelectCommand.MISSINGLOCKER_FORMAT);
+            }
+
+            for (String str : newArgs) {
+                if (str.equals("status")) {
+                    checkStatus = 1;
+                }
+            }
+            if (checkStatus == 0) {
+                for (String str : newArgs) {
+                    if (str.equals("name")) {
+                        throw new DukeException(ExportLockerSelectCommand.MISSINGSTATUS_FORMAT);
+                    }
+                    if (str.equals("matrix")) {
+                        throw new DukeException(ExportLockerSelectCommand.MISSINGSTATUS_FORMAT);
+                    }
+                    if (str.equals("course")) {
+                        throw new DukeException(ExportLockerSelectCommand.MISSINGSTATUS_FORMAT);
+                    }
+                    if (str.equals("email")) {
+                        throw new DukeException(ExportLockerSelectCommand.MISSINGSTATUS_FORMAT);
+                    }
+                    if (str.equals("startdate")) {
+                        throw new DukeException(ExportLockerSelectCommand.MISSINGSTATUS_FORMAT);
+                    }
+                    if (str.equals("enddate")) {
+                        throw new DukeException(ExportLockerSelectCommand.MISSINGSTATUS_FORMAT);
+                    }
+                }
+            }
+            return new ExportLockerSelectCommand(args);
         }
-        if (!args.contains("locker")) {
-            throw new DukeException(ExportLockerSelectCommand.MISSING_FORMAT);
-        }
-        return new ExportLockerSelectCommand(args);
     }
 }
